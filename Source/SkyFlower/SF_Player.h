82fe15efb55927ace8b7bd63028f371a7fa433d9
741CU <>
2024-05-14 16:56:27 +0900

    PlayerController,CharacterBase,Playerクラス作成

---
+++ Source/SkyFlower/SF_Player.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SF_CharacterBase.h"
+#include "SF_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYFLOWER_API ASF_Player : public ASF_CharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SkyFlower/SF_PlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "SF_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKYFLOWER_API ASF_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
