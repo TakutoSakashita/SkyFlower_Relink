da6f29375b3105b3d7b8b4ee406efbd64e800be1
22cu0214 <melposyrup@gmail.com>
2024-05-29 15:14:33 +0900

    SFAフレームワーク作成１/３

---
+++ Source/Asterisk/SFA/SFA_PlayerStateBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../StateMachine/SFR_StateBase.h"
+#include "SFA_PlayerStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTERISK_API USFA_PlayerStateBase : public USFR_StateBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Asterisk/SFA/SFA_PlayerStateMachine.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../StateMachine/SFR_StateMachineComponent.h"
+#include "SFA_PlayerStateMachine.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTERISK_API USFA_PlayerStateMachine : public USFR_StateMachineComponent
+{
+	GENERATED_BODY()
+	
+};
