0aa8c2f477852adbf615af13fd1f1793f40fcf41
DESKTOP-SD25RDT\student <22cu0203@jec.ac.jp>
2024-05-30 11:01:26 +0900

    AIModule追加

--- Source/Asterisk/Asterisk.Build.cs
+++ Source/Asterisk/Asterisk.Build.cs
@@ -1,6 +1,7 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using UnrealBuildTool.Rules;
 
 public class Asterisk : ModuleRules
 {
@@ -8,7 +9,7 @@ public class Asterisk : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
