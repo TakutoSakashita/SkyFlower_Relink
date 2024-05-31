838c1a47444c21bfa06830c02aef0039f00f56e3
741CU <>
2024-05-31 15:01:28 +0900

    データモデルファイルを追加(何も実装していません)

---
+++ Source/Asterisk/DataAsset/SFA_StageDataModel.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SFA_StageDataModel.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASTERISK_API USFA_StageDataModel : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
